6e8804c775f5b6fe34e97ea0f5cf9e0d09daf30f
Daryl C. DeNobrega <80586472+Ddenobrega@users.noreply.github.com>
2022-03-01 01:27:38 -0400

    Fixed Minor Vuln

--- routes/utilities.js
+++ routes/utilities.js
@@ -8,6 +8,9 @@ router.use(express.json())
 
 // Ip lookup Information
 async function getInfo(ip){
+    if(ip == "127.0.0.1" || ip == "0.0.0.0" || ip == localhost){
+        return null
+    }
     const url = `https://ipapi.co/${ip}/json`
     const result = await axios.post(url).then(response => response)
     .then(data => data).catch(err => err)
